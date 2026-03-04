7b0bae1423c1aecc9ff69d76439ccde3a8e8ee81
Mardin Nasouti <info@mardin.cc>
2026-03-04 16:52:19 +0300

    feat(create-task): setting up command handler

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+
+def createTask():
+    title = input("Enter task's title: ")
+    descrption = input("Enter task's descrption: ")
+    status = "TODO"
+    print(f"\n[{status}] {title}: {descrption}")
+
+def editTask():
+    print("editing task")
+
 def main():
-    print("Hello from arsha-web!")
+    commands = {
+        "create-task": createTask,
+        "edit-task": editTask,
+    }
+
+    while True:
+        command = input("-> ")
+        if command in commands.keys():
+            commands[command]()
+
 
 
 if __name__ == "__main__":
