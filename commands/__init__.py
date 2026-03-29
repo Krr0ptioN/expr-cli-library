fe2bd1cbfd29fda3aed5955b91666c56c76159f2
Arsha <arsha.sharify.fakher@gmail.com>
2026-03-30 00:26:57 +0300

    feat(command): removing task (#2)
    * Add remove task feature
    
    * Register remove task command in commands module
    
    ---------
    
    Co-authored-by: Arsha <Arsha.shairfy.faker@gmail.com>

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,9 +1,11 @@
 from .list_task import ListTaskCommand, ListTaskCommandHandler
 from .create_task import CreateTaskCommand, CreateTaskCommandHandler
-
+from .remove_task import RemoveTaskCommand, RemoveTaskCommandHandler
 __all__ = [
     "ListTaskCommand",
     "ListTaskCommandHandler",
     "CreateTaskCommand",
     "CreateTaskCommandHandler",
+    "RemoveTaskCommand",
+    "RemoveTaskCommandHandler"
 ]
